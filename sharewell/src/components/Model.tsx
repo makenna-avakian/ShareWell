05f2064054bb734d8f4544c70243fdcb50d2d749
Makenna Avakian <mavakian@routeware.com>
2024-07-06 16:28:00 -0400

    some more styling

--- sharewell/src/components/Model.tsx
+++ sharewell/src/components/Model.tsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useRef} from "react";
 import useModalStore from "@/stores/useModalStore";
-import modalData from "../data/modalData";
+import modalData from '../data/modalData.json';
+import Image from 'next/image';
+
 
 interface ModalProps {
     title: 'Modal 1' | 'Modal 2' | 'Modal 3';
@@ -42,33 +44,45 @@ const Modal: React.FC<ModalProps> = ({ title }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-      <div ref={modalRef} className="w-[640px] h-[498px] bg-white rounded-xl flex-col justify-start items-center inline-flex">
-    <div className="w-[640px] h-56 bg-indigo-500 justify-center items-center gap-2.5 inline-flex">
+      <div ref={modalRef} className="w-[640px] h-[498px] bg-white rounded-xl flex-col justify-start items-center inline-flex overflow-hidden">
+    <div className="w-[640px] h-56 bg-indigo-500 items-center pl-[48px] pt-[48px] pr-[196px] pb-[43px] shadow-custom inline-flex">
         <div className="flex-col justify-start items-start gap-3 inline-flex">
             <div className="justify-start items-center gap-[106px] inline-flex">
-                <div className="w-[396px] text-white text-[32px] font-medium font-['Poppins'] leading-[44.80px]">{modalInfo.title}</div>
+                <div className="w-[396px] text-white text-[32px] font-['Poppins'] leading-[44.80px]">{modalInfo.title}</div>
             </div>
-            <div className="text-white text-sm font-medium font-['Poppins'] leading-tight">{modalInfo.text}</div>
-            <div className="text-white text-base font-medium font-['Poppins'] leading-snug">{modalInfo.date}<br/>{modalInfo.time}</div>
+            <div className="text-white text-sm  font-['Poppins'] leading-tight">{modalInfo.text}</div>
+            <div className="text-white text-base  font-['Poppins'] leading-snug">{modalInfo.date}<br/>{modalInfo.time}</div>
         </div>
-        
+        <div className="w-[345px] h-[343.80px] pl-10 pr-[38.43px] pt-[45.02px] pb-[44.94px] origin-top-left -rotate-45 justify-center items-center inline-flex">
+<div className="w-[266.55px] h-[253.85px] relative flex flex-col justify-start items-start">
+  <div className="w-[118.99px] h-[158.65px] relative">
+        <img src="/images/greenguy.svg" alt="Blob 1" className="w-[115.74px] h-[157.72px] left-[1.60px] top-[0.26px] absolute"/>
+      </div>
+      <div className="w-[166.59px] h-[199.91px] relative">
+        <img src="/images/yellowguy.svg" alt="Blob 2" className="w-[160.23px] h-[195.15px] left-[1.86px] top-[1.53px] absolute"/>
+     </div>
+     <div className="w-[138.03px] h-[171.35px] relative">
+        <img src="/images/blueguy.svg" alt="Blob 3"  className="w-[137.98px] h-[166.65px] left-[-0px] top-[1.69px] absolute"/>
+        </div>
+    </div>
+    </div>
     </div>
     <div className="self-stretch px-6 pt-6 justify-start items-center gap-2.5 inline-flex">
         <div className="flex-col justify-start items-start inline-flex">
             <div className="flex-col justify-start items-start gap-1 flex">
-            <div className="w-[400px] text-neutral-600 text-sm font-medium font-['Poppins'] leading-tight">Lorem Ipsum</div>
+            <div className="w-[400px] text-neutral-600 text-sm  font-['Poppins'] leading-tight">Lorem Ipsum</div>
                 <div className="self-stretch text-indigo-500 text-base font-bold font-['Poppins'] leading-snug">Lorem Ipsum</div>
             </div>
         </div>
         <div className="w-6 h-6 relative" />
     </div>
     <div className="self-stretch h-20 px-6 pt-4 pb-6 flex-col justify-center items-start gap-4 flex">
-        <div className="self-stretch text-zinc-800 text-sm font-medium font-['Poppins'] leading-tight">{modalInfo.body}</div>
+        <div className="self-stretch text-zinc-800 text-sm  font-['Poppins'] leading-tight">{modalInfo.body}</div>
     </div>
     <div className="self-stretch h-[124px] p-6 border-t border-gray-200 flex-col justify-start items-start gap-2.5 flex">
-        <div className="self-stretch text-center text-neutral-600 text-sm font-normal font-['Poppins'] leading-tight">Lorem Ipsum is simply dummy text.</div>
-        <button className="self-stretch px-8 py-3 bg-teal-500/opacity-10 rounded-xl justify-center items-center gap-2 inline-flex">
-            <div className="text-zinc-800/opacity-20 text-base font-semibold font-['Poppins'] uppercase leading-snug">Lorem Ipsum</div>
+        <div className="w-[592px] text-center text-neutral-600 text-sm font-medium font-['Poppins'] leading-tight">Lorem Ipsum is simply dummy text.</div>
+        <button className="w-[592px] h-[46px] px-8 py-3 bg-teal-500 opacity-10 rounded-xl justify-center items-center gap-2 inline-flex">
+            <div className="text-zinc-800 opacity-20 text-base font-normal font-['Poppins'] uppercase leading-snug">Lorem Ipsum</div>
         </button>
     </div>
 </div>
@@ -77,3 +91,5 @@ const Modal: React.FC<ModalProps> = ({ title }) => {
 };
 
 export default Modal;
+
+
--- sharewell/tailwind.config.ts
+++ sharewell/tailwind.config.ts
@@ -19,9 +19,12 @@ const config: Config = {
       colors: {
         'neutral-600': '#525252',
         'indigo-500': '#687CEB',
-        'zinc-800': '#27272A',
+        'zinc-800': '#282B2D',
         'gray-200': '#E5E7EB',
-        'teal-500': '#14B8A6',
+        'teal-500': '#31A6AD',
+      },
+      boxShadow: {
+        custom: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
       },
     },
   },
